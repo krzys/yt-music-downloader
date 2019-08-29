89ee35964ad445b1223de7302c6636b7738565d0
krzsernik <sternik15@gmail.com>
2019-08-30 00:42:46 +0200

    Upgrade Request class

--- src/com/github/krzsernik/ytmusicdownloader/Request.java
+++ src/com/github/krzsernik/ytmusicdownloader/Request.java
@@ -1,9 +1,7 @@
 package com.github.krzsernik.ytmusicdownloader;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
 import java.util.List;
 
@@ -17,17 +15,41 @@ public class Request {
 
         URL urlObj = new URL(url);
         m_conn = (HttpsURLConnection) urlObj.openConnection();
+        m_conn.setConnectTimeout(5000);
         m_conn.setRequestMethod(method);
+        m_conn.setRequestProperty("Accept", "*/*");
+        m_conn.setRequestProperty("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
+        m_conn.setDoInput(true);
     }
 
     public void setRequestHeader(String name, String value) {
         m_conn.setRequestProperty(name, value);
     }
 
+    public void setCookies(List<HttpCookie> cookies) {
+        StringBuilder sb = new StringBuilder();
+        cookies.forEach(cookie -> {
+            sb.append(cookie.toString() + "; ");
+        });
+        m_conn.setRequestProperty("Cookie", sb.toString());
+    }
+
+    public void setData(String output) throws IOException {
+        m_conn.setDoOutput(true);
+
+        OutputStream os = m_conn.getOutputStream();
+        os.write(output.getBytes("UTF-8"));
+    }
+
     public int send() throws IOException {
         int statusCode = m_conn.getResponseCode();
 
         return statusCode;
+    }
+
+    public HttpsURLConnection getConnection() {
+        return m_conn;
     }
 
     public String getHeader(String name) {
